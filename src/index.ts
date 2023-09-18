9c702c111dec0104fcdfacc343a17a53939121fb
Gabriel Costa de Moraes <121687389+GabrielCM16@users.noreply.github.com>
2023-09-18 09:48:36 -0300

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-import express from "express";
+import path from 'node:path';
+import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+import { router } from './router';
 
-const port = 3003;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+mongoose.connect('mongodb://localhost:27017')
+	.then(() => {
+		const app = express();
+		const port = 3000;
+
+		app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+		app.use(express.json());
+		app.use(router);
+		//teclado de emotion tecla win+.
+		app.listen(port, () => {
+			console.log(`🚗Server is runing on http://localhost:${port}`);
+		});
+	})
+	.catch(() => console.log('Erro ao conectar no mongoDb'));
